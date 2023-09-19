e64227a9b644cad51ae97a51b7d2f905b8b725a9
Keeno <keenosdev@gmail.com>
2023-09-19 10:25:35 +0200

    enhancement
    Making sure that a user isnt able to have the same username and added comments to make the code easier to understand

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,34 +1,62 @@
 <?php
-include "../includes/dbconnection.php"; 
-
 if (isset($_POST["signup"])) {
+    include "../includes/dbconnection.php"; 
 
+    //Grabbing data from form
     $username = $_POST["username"];
     $password = $_POST["password"];
     $email = $_POST["email"];
 
+    // Error handeling 
     if(empty($username) || empty($password) ||  empty($email)) {
         header("location: ../signup.php?error=emptyfields");
         exit();
 
     } else {
-        $sql = "INSERT INTO users (username, password, email)  VALUES (?,?,?)";
+        // Checking to make sure that username doesnt exist in database
+        $sql = "SELECT username FROM users WHERE username=?";
         $stmt = mysqli_stmt_init($conn);
 
-    if (!mysqli_stmt_prepare($stmt,$sql)) {
-        header("location: ../signup.php?error=sqlerror");
-        exit();
+        if (!mysqli_stmt_prepare($stmt,$sql)) {
+            header("location: ../signup.php?error=sqlerror");
+            exit();
+    
+        } else {
+            // Binding string values 
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+
+            // Checking for rows with the same username
+            $resultCheck = mysqli_stmt_num_rows($stmt);
+
+            if ($resultCheck > 0) {
+                header("location: ../signup.php?error=usernametaken&mail=".$email);
+                exit();
+            } else {
+                // Insert from into database after error handeling
+                $sql = "INSERT INTO users (username, password, email)  VALUES (?,?,?)";
+                $stmt = mysqli_stmt_init($conn);
+
+                if (!mysqli_stmt_prepare($stmt,$sql)) {
+                header("location: ../signup.php?error=sqlerror");
+                exit();
 
     } else {
+        // Making sure password is not visible in database
         $hashPwd = password_hash($password, PASSWORD_DEFAULT);
 
+        // Binding string values 
         mysqli_stmt_bind_param($stmt, "sss", $username, $hashPwd, $email);
         mysqli_stmt_execute($stmt);
         header("location: ../signup.php?signup=success");
         exit(); 
     }
+            }
+        }
     }
     } else {
+        // Preventing user from entering the page by accident
         header("location: ../signup.php");
         exit();
     }
