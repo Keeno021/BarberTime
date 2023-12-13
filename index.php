c94cb6b36061b034f9f437170de17226ac8c651d
Keeno <keenosdev@gmail.com>
2023-12-13 13:57:56 +0100

    updates

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 session_start();
 include "includes/header.php";
 include "includes/appointments.inc.php";
+include "includes/calandar.php";
 ?>
 <section class="content">
     <div class="container">
@@ -16,46 +17,12 @@ include "includes/appointments.inc.php";
                 } else {
                     echo '<h4>You are not logged in</h4>';
                 }
+
+                if (isset($_SESSION['admin']) || isset($_SESSION['username'])) {
+                    echo $controls;
+                    echo draw_calendar($month, $year);
+                }
             ?>
-                <?php 
-            if (isset($_SESSION['admin']) || isset($_SESSION['username'])) {
-                ?>
-                <h1 class="mt-5">Create Appointment</h1>
-                <form action="includes/create_appointment.php" method="post">
-                    <div class="form-group">
-                        <label for="username">Name</label>
-                        <input type="text" class="form-control" id="username" name="username"
-                            value="<?php echo $_SESSION['username']; ?>" aria-describedby="username"
-                            placeholder="Enter username" readonly>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="date">Date</label>
-                        <input type="date" class="form-control" id="date" name="date" required>
-                    </div>
-
-                    <!-- Dropdown for Start Time -->
-                    <div class="form-group">
-                        <label for="start_time">Time</label>
-                        <?php
-        // if (empty($is_available)) {
-            // echo "No available time slots for the selected date.";
-        // } else {
-            // Dropdown for available time slots
-            echo '<select class="form-control" id="start_time" name="start_time" required>';
-            foreach ($time_slots as $time_slot) {
-                echo '<option value="' . $time_slot . '">' . $time_slot . '</option>';
-            }
-            echo '</select>';
-        // }
-        ?>
-                    </div>
-                    <button type="submit" name="create_appointment" class="btn btn-primary mt-4">Create
-                        Appointment</button>
-                </form>
-                <?php 
-            }
-        ?>
             </div>
             <div class="col-sm-3"></div>
         </div>
